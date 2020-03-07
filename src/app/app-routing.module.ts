7e65a86a6491a7ff923c7ab8dcd5b91a57e0d7f3
occiandiaali <ocean.diaali@gmail.com>
2020-03-07 14:55:17 +0100

    add routes for new-message & message settings pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,19 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'message-settings',
+    loadChildren: () => import('./message-settings/message-settings.module').then( m => m.MessageSettingsPageModule)
+  },
+  {
+    path: 'new-message',
+    loadChildren: () => import('./new-message/new-message.module').then( m => m.NewMessagePageModule)
   }
+  // {
+  //   path: 'tab3',
+  //   loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
+  // }
 ];
 @NgModule({
   imports: [
