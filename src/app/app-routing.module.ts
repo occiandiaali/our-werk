eef43440f261ff1c67770458c6204e1f833c95d8
occiandiaali <ocean.diaali@gmail.com>
2020-03-10 16:54:59 +0100

    modify routes/routinglink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./our-werk/our-werk.module').then(m => m.OurWerkPageModule)
   },
   {
     path: 'message-settings',
