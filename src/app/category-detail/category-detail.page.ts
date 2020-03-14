8fae6d55d077bc5b901078a44b4eca7dffa449c2
occiandiaali <ocean.diaali@gmail.com>
2020-03-14 17:35:02 +0100

    add placeholder category title

--- src/app/category-detail/category-detail.page.ts
+++ src/app/category-detail/category-detail.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import * as data from '../../assets/icon/data.json';
+
+
 
 @Component({
   selector: 'app-category-detail',
@@ -10,9 +12,10 @@ export class CategoryDetailPage implements OnInit {
 
   workers: any = (data as any).default;
 
+  @Input() category: string = 'Category Title';
 
+  constructor() {  }
 
-  constructor() { }
 
   ngOnInit() {
     console.log(this.workers); 
