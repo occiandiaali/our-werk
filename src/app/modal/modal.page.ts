fe47db9fb623108c5d7073419f98c8dbd41f80c3
occiandiaali <ocean.diaali@gmail.com>
2020-03-14 17:31:53 +0100

    add modal on marker click

---
+++ src/app/modal/modal.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NavParams, ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.page.html',
+  styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit {
+
+  @Input() name: string;
+
+  constructor(navParams: NavParams, public modalCtrl: ModalController) { 
+    console.log(navParams.get(`Worker name: ${name}`));
+  }
+
+  ngOnInit() {
+  }
+
+  onBtnClick() {
+    alert(`Call ${this.name}?`);
+  }
+
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalCtrl.dismiss({
+      'dismissed': true
+    });
+  }
+
+}
