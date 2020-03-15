7ca583bdaab3d8aaa19a45e1cbad95bbea841fb6
occiandiaali <ocean@localhost.localdomain>
2020-03-15 16:08:22 +0100

    bind image url in component and phone no. in alert for testing

--- src/app/modal/modal.page.ts
+++ src/app/modal/modal.page.ts
@@ -9,23 +9,25 @@ import { NavParams, ModalController } from '@ionic/angular';
 export class ModalPage implements OnInit {
 
   @Input() name: string;
+  @Input() imageUrl: string;
+  @Input() telephone: string;
 
-  constructor(navParams: NavParams, public modalCtrl: ModalController) { 
-    console.log(navParams.get(`Worker name: ${name}`));
+  constructor(navParams: NavParams, public modalCtrl: ModalController) {
+    // console.log(navParams.get(`Worker name: ${this.name}`));
   }
 
   ngOnInit() {
   }
 
   onBtnClick() {
-    alert(`Call ${this.name}?`);
+    alert(`Call ${this.name} on ${this.telephone}?`);
   }
 
   dismiss() {
     // using the injected ModalController this page
     // can "dismiss" itself and optionally pass back data
     this.modalCtrl.dismiss({
-      'dismissed': true
+      dismissed: true
     });
   }
 
